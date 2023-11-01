5a01a6925a8b514d0ec28c65db0ff92af37d993a
n0va000 <124531410+n0va000@users.noreply.github.com>
2023-11-01 00:41:05 +0000

    Add files via upload

---
+++ examples/fib.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const previousFib = fibonacci(n - 1);
+  const nextFib = previousFib[n - 2] + previousFib[n - 3];
+  previousFib.push(nextFib);
+
+  return previousFib;
+}
+
+displayOutput(fibonacci(10));
